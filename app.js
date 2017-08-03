f0e1244a2945dffab3139e69c3b27b9bc9a73e31
hanrud <hania.rudy@gmail.com>
2017-08-03 18:17:30 +0200

    add ajax request for current weather

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 $(document).ready(function () {
     var pCity = $('p#city'),
         pTemp = $('p#temperature'),
-        latitude,
-        longitude;
+        lat,
+        long;
 
 
     function getLocation() {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                latitude = position.coords.latitude;
-                longitude = position.coords.longitude;
+            navigator.geolocation.getCurrentPosition(function success(position) {
+                lat = position.coords.latitude;
+                long = position.coords.longitude;
+                console.log(lat, long, position);
+                getWeather();
+
             });
+
         } else {
-            pCity.append('Geolocation not supported');
+            console.log("Geolocation is not supported by this browser.");
         }
     }
 
 
+    function getWeather() {
+        $.ajax({
+            type: 'GET',
+            url: 'https://api.darksky.net/forecast/5c2e09b661c3018b08bd8ac173f93c4f/' + lat + ',' + long,
+            dataType: 'JSONP',
+            success: function (obj) {
+                console.log(obj);
+                console.log(obj.timezone);
+                console.log(toCelsius(obj.currently.temperature));
+            }
+        })
+    }
+    
+    function toCelsius(temp) {
+        return result = Math.round(5/9 * (temp - 32));
+    }
+    
     getLocation();
 
 });
