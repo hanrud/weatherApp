c3aa63f99b0c604e4f58012f3c617afe71f8d101
hanrud <hania.rudy@gmail.com>
2017-08-03 21:23:36 +0200

    add city geolocation from api

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
     var pCity = $('p#city'),
         pTemp = $('p#temperature'),
+        pSummary = $('p#summary'),
+        wind = $('p#other-info span#wind'),
+        pressure = $("p#other-info span#pressure"),
+        Cel = $('a#C'),
+        Fahr = $('a#F'),
         lat,
         long;
 
@@ -10,8 +15,8 @@ $(document).ready(function () {
             navigator.geolocation.getCurrentPosition(function success(position) {
                 lat = position.coords.latitude;
                 long = position.coords.longitude;
-                console.log(lat, long, position);
                 getWeather();
+                getCityName();
 
             });
 
@@ -24,20 +29,42 @@ $(document).ready(function () {
     function getWeather() {
         $.ajax({
             type: 'GET',
-            url: 'https://api.darksky.net/forecast/5c2e09b661c3018b08bd8ac173f93c4f/' + lat + ',' + long,
+            url: 'https://api.darksky.net/forecast//' + lat + ',' + long,
             dataType: 'JSONP',
             success: function (obj) {
-                console.log(obj);
-                console.log(obj.timezone);
-                console.log(toCelsius(obj.currently.temperature));
+                var objRes = obj.currently;
+                
+                pSummary.append(objRes.summary);
+                pTemp.append(toCelsius(objRes.temperature));
+                wind.append(Math.round(objRes.windSpeed));
+                pressure.append(Math.round(objRes.pressure));
+         
+                
+                console.log(obj.currently.icon, obj.currently.summary, obj.currently.windSpeed, obj.currently.pressure)
             }
         })
     }
-    
+
     function toCelsius(temp) {
-        return result = Math.round(5/9 * (temp - 32));
+        return result = Math.round(5 / 9 * (temp - 32));
     }
-    
+
+    function getCityName() {
+        $.ajax({
+            type: 'GET',
+            url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + long + '&key=',
+            success: function (res) {
+                var resultAddress = res.results[0].address_components;
+                pCity.append(resultAddress[3].long_name + ' , ' + 
+                             resultAddress[4].long_name + ' , ' + 
+                             resultAddress[5].long_name);
+            }, 
+            error: function(res) {
+                console.log(res);
+            }      
+        })
+    }
+
     getLocation();
 
 });
