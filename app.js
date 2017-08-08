13132bbb6697e9c4ee295b2757347677b20a11ba
hanrud <hania.rudy@gmail.com>
2017-08-08 21:11:11 +0200

    add weather icons; add styles

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     var pCity = $('p#city'),
         pTemp = $('p#temperature'),
         pSummary = $('p#summary'),
-        wind = $('p#other-info span#wind'),
-        pressure = $("p#other-info span#pressure"),
+        wind = $('p#wind'),
+        pressure = $("p#pressure"),
         Cel = $('a#C'),
         Fahr = $('a#F'),
         lat,
@@ -33,14 +33,47 @@ $(document).ready(function () {
             dataType: 'JSONP',
             success: function (obj) {
                 var objRes = obj.currently;
-                
+
                 pSummary.append(objRes.summary);
                 pTemp.append(toCelsius(objRes.temperature));
-                wind.append(Math.round(objRes.windSpeed));
-                pressure.append(Math.round(objRes.pressure));
-         
+                wind.append(Math.round(objRes.windSpeed) + ' km/h');
+                pressure.append(Math.round(objRes.pressure) + ' hPa');
+
+                console.log(obj.currently.icon, obj.currently.summary, obj.currently.windSpeed, obj.currently.pressure);
+
+                //add icons
+                var skycons = new Skycons({
+                    "color": "#fff"
+                });
+                var icon = document.querySelector('#icon');
+                var objIcon = objRes.icon;
                 
-                console.log(obj.currently.icon, obj.currently.summary, obj.currently.windSpeed, obj.currently.pressure)
+                switch (objIcon) {
+                    case ('partly-cloudy-night'):
+                        skycons.add(icon, Skycons.PARTLY_CLOUDY_NIGHT);
+                        break;
+                    case ('clear-day'):
+                        skycons.add(icon, Skycons.CLEAR_DAY);
+                        break;
+                    case ('clear-night'):
+                        skycons.add(icon, Skycons.CLEAR_NIGHT);
+                        break;
+                    case ('partly-cloudy-day'):
+                        skycons.add(icon, Skycons.PARTLY_CLOUDY_DAY);
+                        break;
+                    case ('cloudy'):
+                        skycons.add(icon, Skycons.CLOUDLY);
+                        break;
+                    case ('rain'):
+                        skycons.add(icon, Skycons.RAIN);
+                        break;
+                    case ('sleet'):
+                        skycons.add(icon, Skycons.SLEET);
+                        break;
+                    case ('snow'):
+                        
+                }
+                skycons.play();
             }
         })
     }
@@ -55,13 +88,13 @@ $(document).ready(function () {
             url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + long + '&key=',
             success: function (res) {
                 var resultAddress = res.results[0].address_components;
-                pCity.append(resultAddress[3].long_name + ' , ' + 
-                             resultAddress[4].long_name + ' , ' + 
-                             resultAddress[5].long_name);
-            }, 
-            error: function(res) {
+                pCity.append(resultAddress[3].long_name + ' , ' +
+                    resultAddress[4].long_name + ' , ' +
+                    resultAddress[5].long_name);
+            },
+            error: function (res) {
                 console.log(res);
-            }      
+            }
         })
     }
 
