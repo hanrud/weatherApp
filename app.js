6c689ff082f6475c7a8d6f4a9d7878f89f86058c
hanrud <hania.rudy@gmail.com>
2017-08-08 21:49:51 +0200

    add different backgrounds for different weather

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     var pCity = $('p#city'),
         pTemp = $('p#temperature'),
         pSummary = $('p#summary'),
+        body = $('body'),
         wind = $('p#wind'),
         pressure = $("p#pressure"),
         Cel = $('a#C'),
@@ -47,31 +48,48 @@ $(document).ready(function () {
                 });
                 var icon = document.querySelector('#icon');
                 var objIcon = objRes.icon;
-                
+
                 switch (objIcon) {
                     case ('partly-cloudy-night'):
                         skycons.add(icon, Skycons.PARTLY_CLOUDY_NIGHT);
+                        body.css('background-image', "url('http://images.all-free-download.com/images/graphiclarge/cloudy_night_moon_238791.jpg')");
+                        $('div.main').css('background-color', 'rgba(14, 43, 60, 0.61)');
                         break;
                     case ('clear-day'):
                         skycons.add(icon, Skycons.CLEAR_DAY);
+                        body.css('background-image', "url('https://image.freepik.com/free-photo/starfish-on-summer-sunny-beach-at-ocean-background-image-travel-vacation-concepts_1423-276.jpg')");
+                        $('div.main').css('background-color', 'rgba(197, 120, 96, 0.6)');
                         break;
                     case ('clear-night'):
                         skycons.add(icon, Skycons.CLEAR_NIGHT);
+                        body.css('background-image', "url('https://image.freepik.com/free-photo/modern-city-at-night_1112-856.jpg')");
+                        $('div.main').css('background-color', 'rgba(14, 43, 60, 0.61)');
                         break;
                     case ('partly-cloudy-day'):
                         skycons.add(icon, Skycons.PARTLY_CLOUDY_DAY);
+                        body.css('background-image', 'url("http://images.freeimages.com/images/large-previews/294/partly-cloudy-1173077.jpg")');
+                        $('div.main').css('background-color', 'rgba(14, 43, 60, 0.61)');
                         break;
                     case ('cloudy'):
-                        skycons.add(icon, Skycons.CLOUDLY);
+                        skycons.add(icon, Skycons.CLOUDY);
+                        body.css('background-image', "url('https://image.freepik.com/free-photo/black-rain-abstract-dark-power_1127-2380.jpg')");
+                        $('div.main').css('background-color', 'rgba(14, 43, 60, 0.61)');
                         break;
                     case ('rain'):
                         skycons.add(icon, Skycons.RAIN);
+                        body.css('background-image', "url('https://cdn.pixabay.com/photo/2017/06/13/20/42/weather-2400074_960_720.jpg')");
+                        $('div.main').css('background-color', 'rgba(30,48,36, 0.61)');
                         break;
                     case ('sleet'):
                         skycons.add(icon, Skycons.SLEET);
+                        body.css('background-image', "url('https://cdn.pixabay.com/photo/2015/01/07/05/26/ice-591137_960_720.jpg')");
+                        $('div.main').css('background-color', 'rgba(124,115,108, 0.7)');
                         break;
                     case ('snow'):
-                        
+                        skycons.add(icon, Skycons.SNOW);
+                        body.css('background-image', "url('https://image.freepik.com/free-photo/christmas-snowy-landscape_1048-2833.jpg')");
+                        $('div.main').css('background-color', 'rgba(73, 104, 130, 0.7)');
+                        break;
                 }
                 skycons.play();
             }
